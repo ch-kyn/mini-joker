28cee8ba0fce089df110c48d92505e1ffc0d209c
ch-kyn <chriskng@stud.ntnu.no>
2026-07-10 02:27:22 +0200

    Render config

--- web/server.js
+++ web/server.js
@@ -44,6 +44,12 @@ const shell = pty.spawn(gamePath, [], {
     });
 });
 
-server.listen(3000, () => {
-    console.log("http://localhost:3000");
-});
+// server.listen(3000, () => {
+//     console.log("http://localhost:3000");
+// });
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () =>
+    console.log(`Server running on ${PORT}`)
+);
